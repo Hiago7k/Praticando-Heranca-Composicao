d7c34028436eb37936ece0894a4ed82048eb02fb
Hiago 7k <hiago7k@gmail.com>
2026-03-04 19:03:32 -0300

    instanciando objetos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Praticando_Heranca_Composicao;
+
+ClienteVip cliente1 = new ClienteVip("Renata", 32, "Ouro", "VIP123A");
+ClienteVip cliente2 = new ClienteVip("Leonardo", 40, "Diamante", "VIP789X");
